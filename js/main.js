5da96b3ba91aa5d7b5afeb39680ed97d2105addf
rancongjie <95633676@qq.com>
2016-02-22 11:22:51 +0800

    button3d

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ main.init = function () {
   main.configNavAnimate();
   main.events();
   $('body').height(8500);
+  main.button3D('.start', '.state1', '.state2', 0.3);
 };
 
 $(document).ready(main.init);
@@ -51,7 +52,7 @@ main.nav = function () {
   var languageAnimate = new TimelineMax();
   $('.language').bind('mouseenter', function () {
     languageAnimate.clear();
-    languageAnimate.to('.dropdown', 0.5, {opacity: 1, 'display': 'block'});
+    languageAnimate.to('.dropdown', 0.7, {opacity: 1, 'display': 'block'});
   });
   $('.language').bind('mouseleave', function () {
     languageAnimate.clear();
@@ -59,8 +60,33 @@ main.nav = function () {
   })
 
 };
-main.button3D = function () {
+main.button3D = function (obj, element1, element2, d) {
   var buttonAnimate = new TimelineMax();
+  buttonAnimate.to($(obj).find(element1), 0, {
+    rotationX: 0,
+    transformPerspective: 600,
+    transformOrigin: 'center bottom'
+  });
+  buttonAnimate.to($(obj).find(element2), 0, {
+    rotationX: -90,
+    transformPerspective: 600,
+    transformOrigin: 'top center'
+  });
+
+  $(obj).bind('mouseenter', function () {
+    var enterAnimate = new TimelineMax();
+
+    enterAnimate.to($(this).find(element1), d, {rotationX: 90, top: -$(this).find(element1).height()}, 0);
+    enterAnimate.to($(this).find(element2), d, {rotationX: 0, top: 0}, 0);
+
+  });
+  $(obj).bind('mouseleave', function () {
+    var leaveAnimate = new TimelineMax();
+
+    leaveAnimate.to($(this).find(element1), d, {rotationX: 0, top: 0}, 0);
+    leaveAnimate.to($(this).find(element2), d, {rotationX: -90, top: $(this).find(element2).height()}, 0);
+
+  });
 
 };
 main.events = function () {
